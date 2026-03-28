2503405c470a7c098e240383dd038ed4fdb04420
Tuan Anh <victory1080@gmail.com>
2026-03-28 11:56:43 +0700

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="vi" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
